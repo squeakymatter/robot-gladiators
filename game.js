6f2f8b5ff464b69e1a11ea0be5910b52c3a9f597
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2020-12-11 23:54:52 -0800

    starting 3-5 over...

--- game.js
+++ game.js
@@ -11,7 +11,6 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-      
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -172,29 +171,13 @@ var randomNumber = function(min, max) {
 
   return value;
 };
-
-// function to set name
-var getPlayerName = function() {
-  var name = "";
-
-  while (name === "" || name === null) {
-    name = prompt("What is your robot's name?");
-  }
-
-  console.log(`Your robot's name is "" + name`);
-  return name;
-};
-
-
-
 /* END GAME FUNCTIONS */
 
 /* GAME INFORMATION / VARIABLES */
 
-
 // player information
 var playerInfo = {
-  name: getPlayerName(),
+  name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
   money: 10,
